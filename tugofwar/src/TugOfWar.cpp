f66150e6a78ebbe9ac290af2309a585f2243c85b
Scott Wiedemann <lemmingapex@gmail.com>
2016-06-09 09:43:25 -0600

    tug of war fixes.

--- tugofwar/src/TugOfWar.cpp
+++ tugofwar/src/TugOfWar.cpp
@@ -4,12 +4,11 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <limits>
 #include <algorithm>
 
 using namespace std;
 
-const int INF = numeric_limits<int>::max();
+const int INF = 450*100;
 
 // holds the weights of at-most 100 people for each case
 int weights[100];
@@ -37,14 +36,14 @@ int main() {
 		int W = totalWeight + 1;
 
 		bool subsumExist[H][W];
-		int subsumCache[H][100];
+		int subsumCache[H][15000];
 
 		// set array to false
 		for(int i=0; i<H; i++) {
 			for(int j=0; j<W; j++) {
 				subsumExist[i][j] = false;
 			}
-			for(int j=0; j<100; j++) {
+			for(int j=0; j<15000; j++) {
 				subsumCache[i][j] = 0;
 			}
 		}
@@ -59,14 +58,14 @@ int main() {
 			int iWeight = weights[i];
 			for(int j = i; j >= 0; j--) {
 
-				// if(j>N/2) {
-				// 	j=(N/2);
-				// }
-				//
-				// // this is an optimization to break early, becauase we don't need to fill out the entire table!
-				// if(i > centerPerson && (N/2) - j > N - i) {
-				// 	break;
-				// }
+				if(j>N/2) {
+					j=(N/2);
+				}
+
+				// this is an optimization to break early, becauase we don't need to fill out the entire table!
+				if(i > centerPerson && (N/2) - j > N - i) {
+					break;
+				}
 
 				for(int indexAtJ=subsumCache[j][0]; indexAtJ>0; indexAtJ--) {
 					int cachedWeight = subsumCache[j][indexAtJ];
@@ -104,7 +103,7 @@ int main() {
 		// get the answer
 		for (int i = subsumCache[centerPerson][0]; i>0; i--) {
 			int weight1 = subsumCache[centerPerson][i];
-			if(weight1 <= W && subsumExist[centerPerson][weight1]) {
+			if(subsumExist[centerPerson][weight1]) {
 				int weight2 = totalWeight - weight1;
 				int tWeightDiff = abs(weight1 - weight2);
 				if(tWeightDiff < weightDiff) {
