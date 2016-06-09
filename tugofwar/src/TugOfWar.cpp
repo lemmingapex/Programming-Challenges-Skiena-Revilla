f2081986d38d1348d8cf05cb3b91316e58e2809f
Scott Wiedemann <lemmingapex@gmail.com>
2016-06-08 20:59:50 -0600

    Using plain array instead of set.

--- tugofwar/src/TugOfWar.cpp
+++ tugofwar/src/TugOfWar.cpp
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <iostream>
 #include <limits>
-#include <unordered_set>
 #include <algorithm>
 
 using namespace std;
@@ -36,43 +35,59 @@ int main() {
 
 		int H = N + 1;
 		int W = totalWeight + 1;
-		// array of hash sets
-		unordered_set<int> *subsumCache = new unordered_set<int>[H];
-		for(int i = 0; i < H; i++) {
-			unordered_set<int> temp;
-			subsumCache[i] = temp;
+
+		bool subsumExist[H][W];
+		int subsumCache[H][100];
+
+		// set array to false
+		for(int i=0; i<H; i++) {
+			for(int j=0; j<W; j++) {
+				subsumExist[i][j] = false;
+			}
+			for(int j=0; j<100; j++) {
+				subsumCache[i][j] = 0;
+			}
 		}
 
+		subsumCache[0][0] = 1;
 		// 0 people, weight a total of 0.
-		subsumCache[0].insert(0);
+		subsumCache[0][1] = 0;
+		subsumExist[0][0] = true;
 
 		for(int i = 0; i < N; i++) {
 			// get a persons weight
 			int iWeight = weights[i];
 			for(int j = i; j >= 0; j--) {
-				unordered_set<int>::iterator it;
-				for (it = subsumCache[j].begin(); it != subsumCache[j].end(); it++) {
-					int cachedWeight = *it;
+
+				// if(j>N/2) {
+				// 	j=(N/2);
+				// }
+				//
+				// // this is an optimization to break early, becauase we don't need to fill out the entire table!
+				// if(i > centerPerson && (N/2) - j > N - i) {
+				// 	break;
+				// }
+
+				for(int indexAtJ=subsumCache[j][0]; indexAtJ>0; indexAtJ--) {
+					int cachedWeight = subsumCache[j][indexAtJ];
 					int tWeight = iWeight + cachedWeight;
 
 					if(tWeight > W) {
 						continue;
 					}
 
-					// this is an optimization to break early, becauase we don't need to fill out the entire table!
-					if(i > centerPerson + 1 && (N/2) - j > N - i) {
-						break;
-					}
+					if(subsumExist[j][cachedWeight] == true) {
+						if(subsumExist[j+1][tWeight] == false) {
 
-					if(subsumCache[j].count(cachedWeight)) {
-						if(!subsumCache[j+1].count(tWeight)) {
-							subsumCache[j+1].insert(tWeight);
+							subsumExist[j+1][tWeight]=true;
+							subsumCache[j+1][0]++;
+							subsumCache[j+1][subsumCache[j+1][0]]=tWeight;
 
 							// printf("subsumCache:\n");
-							// unordered_set<int>::iterator iit;
+							//
 							// for (int ii = 0; ii < H; ++ii) {
-							// 	for (iit = subsumCache[ii].begin(); iit != subsumCache[ii].end(); iit++) {
-							// 		printf("%d ", *iit);
+							// 	for (int jj = subsumCache[ii][0]; jj > 0; jj--) {
+							// 		printf("%d ", subsumCache[ii][jj]);
 							// 	}
 							// 	printf("\n");
 							// }
@@ -83,30 +98,28 @@ int main() {
 			}
 		}
 
-		int team1Weight;
-		int team2Weight;
+		int team1Weight = 0;
 		int weightDiff = INF;
 
 		// get the answer
-		unordered_set<int>::iterator it;
-		for (it = subsumCache[centerPerson].begin(); it != subsumCache[centerPerson].end(); it++) {
-			int weight1 = *it;
-			int weight2 = totalWeight - weight1;
-			int tWeightDiff = abs(weight1 - weight2);
-			if(tWeightDiff < weightDiff) {
-				weightDiff = tWeightDiff;
-				team1Weight = min(weight1, weight2);
-				team2Weight = max(weight1, weight2);
+		for (int i = subsumCache[centerPerson][0]; i>0; i--) {
+			int weight1 = subsumCache[centerPerson][i];
+			if(weight1 <= W && subsumExist[centerPerson][weight1]) {
+				int weight2 = totalWeight - weight1;
+				int tWeightDiff = abs(weight1 - weight2);
+				if(tWeightDiff < weightDiff) {
+					weightDiff = tWeightDiff;
+					team1Weight = min(weight1, weight2);
+				}
 			}
 		}
 
-		printf("%d %d\n", team1Weight, team2Weight);
+		printf("%d %d\n", team1Weight, totalWeight - team1Weight);
 
 		if(c < NCases - 1) {
 			printf("\n");
 		}
 
-		delete [] subsumCache;
 	}
 	return 0;
 }
