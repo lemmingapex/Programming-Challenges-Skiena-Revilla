f55c52018eeb06d152fac2a9c4f803fec794c9e3
Scott Wiedemann <lemmingapex@gmail.com>
2020-12-24 10:42:01 -0700

    10.5.3 Firestation.

---
+++ Chapter-10/10-5-3-fire-station/README.md
@@ -0,0 +1,10 @@
+# [Fire Station](https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1219)
+
+## Thoughts
+
+
+## Compile
+`make`
+
+## Run
+`./fire-station < ./sample_input.txt`
--- Chapter-10/10-5-3-fire-station/src/fire_station.cpp
+++ Chapter-10/10-5-3-fire-station/src/fire_station.cpp
@@ -1,15 +1,9 @@
-// Scott Wiedemann
-// 10/06/2010
-// firestation.cpp
-// CSCI562-A Applied Algorithms and Data Structures
-
 #include <fstream>
 #include <vector>
 
 using namespace std;
 
-class Road
-{
+class Road {
 	public:
 		int intersection1;
 		int intersection2;
@@ -19,8 +13,7 @@ class Road
 		Road()
 		{ }
 
-		Road(int I1, int I2, int D)
-		{
+		Road(int I1, int I2, int D) {
 			intersection1=I1;
 			intersection2=I2;
 
@@ -28,14 +21,12 @@ class Road
 		}
 };
 
-class Intersection
-{
+class Intersection {
 	public:
 		vector<Road> roads;
 		int fsDist;
 
-		Intersection()
-		{
+		Intersection() {
 			fsDist=0;
 		}
 };
@@ -45,50 +36,40 @@ int firestations[100];
 Intersection intersections[501];
 Intersection tempIntersections[501];
 
-void updateFsDistance(int Inter, int distance)
-{
+void updateFsDistance(int Inter, int distance) {
 	tempIntersections[Inter].fsDist = distance;
-	for(int i=0; i < tempIntersections[Inter].roads.size(); i++)
-	{
+	for (int i=0; i < tempIntersections[Inter].roads.size(); i++) {
 		Road CurrnetInter = tempIntersections[Inter].roads[i];
-		if(distance + CurrnetInter.distance < tempIntersections[CurrnetInter.intersection1].fsDist)
-		{
+		if (distance + CurrnetInter.distance < tempIntersections[CurrnetInter.intersection1].fsDist) {
 			updateFsDistance(CurrnetInter.intersection1,distance + CurrnetInter.distance);
 		}
 	}
 }
 
-int biggestDistance()
-{
+int biggestDistance() {
 	int biggest=0;
-	for(int i=0; i < NIntersections; i++)
-	{
-		if (tempIntersections[i].fsDist > biggest)
-		{
+	for (int i=0; i < NIntersections; i++) {
+		if (tempIntersections[i].fsDist > biggest) {
 			biggest = tempIntersections[i].fsDist;
 		}
 	}
 	return biggest;
 }
 
-void clearStructs()
-{
-	for(int i=1; i<NIntersections; i++)
-	{
+void clearStructs() {
+	for (int i=1; i<NIntersections; i++) {
 		intersections[i].fsDist = tempIntersections[i].fsDist = 999999;
 		intersections[i].roads.clear();
 		tempIntersections[i].roads.clear();
 	}
 
-	for(int i=1; i<NFirestations; i++)
-	{
+	for(int i=1; i<NFirestations; i++) {
 		firestations[i] = 0;
 	}
 }
 
 
-void parseInput()
-{
+void parseInput() {
 	scanf("%d",&NFirestations);
 	scanf("%d",&NIntersections);
 	NIntersections += 1;
@@ -96,18 +77,15 @@ void parseInput()
 	clearStructs();
 
 	int fires;
-	for(int i=0; i<NFirestations; i++)
-	{
+	for(int i=0; i<NFirestations; i++) {
 		scanf("%d",&fires);
 		firestations[i] = fires;
 	}
 }
 
 
-void findDistance()
-{
-	if(NIntersections<3)
-	{
+void findDistance() {
+	if (NIntersections < 3) {
 		printf("1\n");
 		return;
 	}
@@ -115,8 +93,7 @@ void findDistance()
 	int firstInter, secondInter, dist;
 	char line[1024];
 	gets(line);
-	while(gets(line) && *line)
-	{
+	while (gets(line) && *line) {
 		sscanf(line, "%d %d %d", &firstInter, &secondInter, &dist);
 
 		Road Road1=Road(firstInter,secondInter,dist);
@@ -126,47 +103,39 @@ void findDistance()
 		tempIntersections[secondInter].roads.push_back(Road1);
 	}
 
-	for(int i=0; i<NFirestations; i++)
-	{
+	for (int i=0; i<NFirestations; i++) {
 		updateFsDistance(firestations[i], 0);
 	}
-	
-	for(int i=1; i<NIntersections; i++)
-	{
+
+	for (int i=1; i<NIntersections; i++) {
 		intersections[i] = tempIntersections[i];
 	}
-	
+
 	int smallestDistance = 99999;
 	int smallestInter, tempd;
-	for(int i=1; i<NIntersections; i++)
-	{
+	for (int i=1; i<NIntersections; i++) {
 		updateFsDistance(i, 0);
 		tempd=biggestDistance();
-		if(tempd < smallestDistance)
-		{
+		if (tempd < smallestDistance) {
 			smallestDistance = tempd;
 			smallestInter = i;
 		}
 
-		for(int i=1; i < NIntersections; i++)
-		{
+		for (int i=1; i < NIntersections; i++) {
 			tempIntersections[i] = intersections[i];
 		}
 	}
-        printf("%d\n", smallestInter);
+	printf("%d\n", smallestInter);
 	return;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, const char *argv[]) {
 	int NCases;
 	scanf("%d",&NCases);
-	for(int i=0; i<NCases; i++)
-	{
+	for (int i=0; i<NCases; i++) {
 		parseInput();
 		findDistance();
-		if(i<NCases-1)
-		{
+		if(i < NCases - 1) {
 			printf("\n");
 		}
 	}
